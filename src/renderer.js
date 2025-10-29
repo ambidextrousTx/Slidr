b18e804438b3ad73b65c3ae5eb73b15b80463672
Ravi Sinha <ravisinha@my.unt.edu>
2025-10-28 21:24:50 -0500

    Add shuffle functionality

--- src/renderer.js
+++ src/renderer.js
@@ -30,8 +30,9 @@ function shuffleArray(array) {
   console.log(array)
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-
-    // [array[i], array[j]] = [array[j], array[i]]
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
 }
 
